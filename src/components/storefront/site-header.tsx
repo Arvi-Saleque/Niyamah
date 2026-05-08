fe79b715c13f7d6bceb7bf954e5c3eaff69f6b2d
Arvi-saleque <alifsalek.as@gmail.com>
2026-05-08 14:18:51 +0600

    fix: remove search overlay focus outline

--- src/components/storefront/site-header.tsx
+++ src/components/storefront/site-header.tsx
@@ -414,6 +414,7 @@ function SearchOverlay({
           autoFocus={open}
           placeholder="What are you looking for?"
           className="h-11 flex-1 rounded-none border-0 bg-transparent text-[15px] outline-none ring-0 placeholder:text-black/55 focus:border-0 focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
+          style={{ outline: "none", outlineOffset: 0, boxShadow: "none" }}
         />
         {query && (
           <button
