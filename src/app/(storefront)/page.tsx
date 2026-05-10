639544e2e23aae92353c1dafa26a7044602d3890
Arvi-saleque <alifsalek.as@gmail.com>
2026-05-10 16:43:46 +0600

    review section added

--- src/app/(storefront)/page.tsx
+++ src/app/(storefront)/page.tsx
@@ -57,13 +57,15 @@ export default async function StorefrontHomePage() {
       {/* <TrustPromiseStrip /> */}
       {/* <ShopByPurposeSection /> */}
       {/* <FeaturedCollectionsSection /> */}
-      <ProductStoryRailSection
+      {
+      /*<ProductStoryRailSection
         products={storyProducts}
         eyebrow="Curated essentials"
         title="Signature Picks from Niyamah"
         subtitle="A focused edit of Quran, prayer essentials, books, and gifts with the richer book-opening product card treatment."
         ctaLabel="Shop Signature Picks"
-      />
+      />*/
+      }
       {/* <section className="bg-[#fffaf0] py-16 sm:py-20">
         <div className="mx-auto w-full max-w-[1500px] px-4 sm:px-6 lg:px-8">
           <GiftBuilderSection whatsappPhone="8801760982072" />
